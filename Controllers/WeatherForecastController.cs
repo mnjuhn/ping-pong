7b8b5fed14af62ea63c7371f8d5e08312553b802
unknown <mnjuhn@gmail.com>
2019-10-28 22:38:23 -0400

    Adding Angualr for list of players

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -34,15 +34,6 @@ namespace PingPong.Controllers
       var foo = players.FirstOrDefault<Player>();
       return players;
 
-            /*return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                //Summary = Summaries[rng.Next(Summaries.Length)]
-                Summary = foo.FirstName
-            })
-            .ToArray();*/
-
         }
     }
 }
